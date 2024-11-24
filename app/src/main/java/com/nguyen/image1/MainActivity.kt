643b970573752a07ee617b8d4ca8e8d873123e25
Mỹ Nguyễn <nguyen@MacBook-Pro-M1Max-32G-1T.local>
2024-11-24 12:02:03 -0800

    b1: Baseline, using Glide

---
+++ app/src/main/java/com/nguyen/image1/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.nguyen.image1
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.bumptech.glide.Glide
+import com.nguyen.image1.databinding.ActivityMainBinding
+
+private const val TAG = "MainActivity"
+private const val URL = "https://rkpandey.com/images/rkpDavidson.jpg"
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        Log.d(TAG, "Loading image from URL into ImageView")
+        Glide.with(this).load(URL).into(binding.image)
+    }
+}
